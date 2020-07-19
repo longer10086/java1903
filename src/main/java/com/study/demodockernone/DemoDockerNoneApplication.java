82b06b41f55a8bb2b25794e6047ce008946bc209
jack.yan <1831872962@qq.com>
2020-07-19 20:30:18 +0800

    第一次提交

---
+++ src/main/java/com/study/demodockernone/DemoDockerNoneApplication.java
@@ -0,0 +1,13 @@
+package com.study.demodockernone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerNoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDockerNoneApplication.class, args);
+    }
+
+}
